6513cf86a31fc78f03b046f05365f1ac969a46c2
chiang-sh <b228103049@tmu.edu.tw>
2026-06-23 13:10:40 +0800

    init: first commit

---
+++ src/main/java/io/github/chiang_sh/file_nest/FileNestApplication.java
@@ -0,0 +1,13 @@
+package io.github.chiang_sh.file_nest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/chiang_sh/file_nest/FileNestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.chiang_sh.file_nest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
