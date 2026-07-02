abda454a96522a847a7def484781dcddd8a6dc97
chiang-sh <b228103049@tmu.edu.tw>
2026-07-02 13:26:34 +0800

    feat: add Swagger for API documentation

--- src/main/java/io/github/chiang_sh/file_nest/security/AuthController.java
+++ src/main/java/io/github/chiang_sh/file_nest/security/AuthController.java
@@ -1,5 +1,7 @@
 package io.github.chiang_sh.file_nest.security;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,16 +9,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "Authorization")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
